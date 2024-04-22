ee9f9988027db8b309b7dbb1c0c0a99b25a3fb0f
Robert Trifan <trifangrobert@gmail.com>
2024-04-23 01:09:02 +0300

    fetch not working, I need sleep

--- frontend/src/pages/AvailableCars.js
+++ frontend/src/pages/AvailableCars.js
@@ -1,5 +1,24 @@
-const AvailableCars = () => {
-  return <div>All available cars</div>;
-};
+import React from 'react';
+import { useAvailableCars } from '../hooks/useAvailableCars';  // Adjust path as necessary
 
-export default AvailableCars;
+function AvailableCarsList() {
+    const { cars, loading, error } = useAvailableCars();
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <div>
+            <h1>Available Cars</h1>
+            <ul>
+                {cars.map(car => (
+                    <li key={car.tokenId}>
+                        Token ID: {car.tokenId}, Price: {car.price} WEI
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default AvailableCarsList;
