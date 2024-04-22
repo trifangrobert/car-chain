61d6c43fe539cef3aedda884e07cc651c9286200
Robert Trifan <trifangrobert@gmail.com>
2024-04-23 00:54:02 +0300

    connect metamask wallet

--- frontend/src/pages/ConnnectWallet.js
+++ frontend/src/pages/ConnnectWallet.js
@@ -1,9 +1,27 @@
-const ConnectWallet = () => {
-    return (  
-        <div>
-            Connect your wallet
-        </div>
-    );
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function ConnectWallet() {
+  let navigate = useNavigate();
+
+  async function connectWalletHandler() {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        navigate('/available-cars');
+      } catch (error) {
+        console.error("Failed to connect wallet", error);
+      }
+    } else {
+      alert('Please install MetaMask!');
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={connectWalletHandler}>Connect Wallet</button>
+    </div>
+  );
 }
- 
+
 export default ConnectWallet;
